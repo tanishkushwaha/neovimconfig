ce6d20558f961c525d9e82566d7865dd6c8808bd
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-09-13 10:00:35 +0530

    Initial commit

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "prettier",
+      "typescript-language-server",
+      "eslint-lsp",
+      "tailwindcss-language-server",
+      "gopls",
+      "golangci-lint",
+    },
+  },
+}
